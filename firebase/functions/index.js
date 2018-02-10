e4dcd8a4f29d4c0826dc43c4cd0d8a42c9fbe9d1
dbishop <dbishop.rwc@gmail.com>
2018-02-09 20:32:37 -0500

    stub of adding new spells

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,6 +1,7 @@
 const functions = require('firebase-functions');
 const admin = require("firebase-admin");
 const express = require("express");
+const app = express();
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -8,27 +9,38 @@ const express = require("express");
 admin.initializeApp(functions.config().firebase);
 var db = admin.firestore();
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
+// Allow cross-origin requests
+app.use(cors({origin: true}));
+
+app.get('/', (request, response) => {
   response.send("Hello from Firebase!");
 });
 
-exports.allSpells = functions.https.onRequest((request, response) => {
-  var res = db.collection("spells").get()
+app.get('/spells', (request, response) => {
+  db.collection("spells").get()
     .then(snapshot => {
-        var all_spells = [];
-        snapshot.forEach(doc => {
-          console.log(doc.id);
-          all_spells.push({
-            id: doc.id,
-            data: doc.data()
-          });
+      var all_spells = [];
+      snapshot.forEach(doc => {
+        all_spells.push({
+          id: doc.id,
+          data: doc.data()
         });
-        console.log("inside .then()", all_spells);
-        return all_spells;
+      });
+      return response.send(all_spells);
     })
     .catch(err => {
-        console.error(err);
+      console.error(err);
     });
-
-  response.send(res);
 });
+
+app.post('/spells', (request, response) => {
+  var spells = request.body.spells;
+  spells.forEach(spell => {
+    db.collection("spells").add({
+      name: spell.name,
+      level: spell.level
+    });
+  });
+});
+
+exports.widgets = functions.https.onRequest(app);
