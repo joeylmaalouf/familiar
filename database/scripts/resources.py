706d89b2ff24d295cfcd302ae170527ce990d525
Joey L. Maalouf <joeylmaalouf@gmail.com>
2018-02-13 18:13:14 -0500

    18: added arcane discovery parsing and tweaked scraper

--- database/scripts/resources.py
+++ database/scripts/resources.py
@@ -1,7 +1,13 @@
+import re
+
 # thanks, AoN!
 BASE_URL = r'http://archivesofnethys.com/'
 
-# spells
+
+# ------------------------------------------------------------------------------
+# all spells
+# ------------------------------------------------------------------------------
+
 SPELL_DEFAULTS = {
     'casttime'   : None,
     'components' : None,
@@ -10,15 +16,17 @@ SPELL_DEFAULTS = {
     'link'       : None,
     'longdesc'   : None,
     'name'       : None,
-    'restriction': None,
     'range'      : None,
+    'restriction': None,
     'save'       : None,
     'school'     : None,
     'shortdesc'  : None,
     'spellres'   : None,
     'target'     : None
 }
+
 SPELL_LIST_REGEX = r'<b><a href="(?P<link>.*?)">(?:<img.*?>\s?){0,2}(?P<name>.*?)(?:</b><sup>.</sup><b>)*</a></b>: (?P<shortdesc>.*?)<br />'
+
 SPELL_REGEXES = [
     r'<h1 class="title">(?:<img.*?>)* ?(?P<name>.*?)</h1>',
     r'<b>School</b> (?P<school>.*?)(?:;|<)',
@@ -32,3 +40,96 @@ SPELL_REGEXES = [
     r'<b>Spell Resistance</b> (?P<spellres>.*?)<',
     r'>Description</h3>(?P<longdesc>.*?)</?(?:span|h1|h2)'
 ]
+
+def process_spell(spell):
+    """ Processes spell data into a more usable format. """
+
+    # strip any leading and trailing whitespace in all of our spell's properties
+    for prop in spell:
+        if spell[prop]:
+            spell[prop] = spell[prop].strip()
+
+    # turn a spell level string into a dict of classes and spell levels
+    level_regex = re.compile(r'(?P<class0>.+?)(?:/(?P<class1>.+))? (?P<level>\d+)')
+    level_dict = {}
+    for level_pair in spell['level'].split(', '):
+        results = level_regex.search(level_pair)
+        if results:
+            match = results.groupdict()
+            level_dict[match['class0']] = int(match['level'])
+            if match['class1']:
+                level_dict[match['class1']] = int(match['level'])
+    spell['level'] = level_dict
+
+    # turn a components string into a list of components and a dict of any accompanying details
+    component_split_regex = re.compile(r', (?![^()]*\))')
+    component_regex = re.compile(r'(?P<type>\w+)(?: \((?P<details>.+?)\))?')
+    components_list = []
+    components_dict = {}
+    components = component_split_regex.split(spell['components'])
+    for component in components:
+        results = component_regex.search(component)
+        if results:
+            match = results.groupdict()
+            components_list.append(match['type'])
+            if match['details'] is not None:
+                components_dict[match['type']] = match['details']
+    spell['components'] = {
+        'list': components_list,
+        'details': components_dict
+    }
+
+    return spell
+
+
+# ------------------------------------------------------------------------------
+# wizard arcane discoveries
+# ------------------------------------------------------------------------------
+
+ARCANE_DISCOVERY_DEFAULTS = {
+    'description': None,
+    'name'       : None,
+    'restriction': None,
+    'spectype'   : None
+}
+
+ARCANE_DISCOVERY_REGEX = r'<td>\s*<span.*?><b>(?:<img.*?>\s*)*(?P<name>.*?)\s*(?:\((?P<spectype>Ex|Su|Sp)\))?</b>.*?\): (?P<description>.*?(?P<restriction>You must.*?to (select|choose) this discovery\.)?)<hr /></span>\s*</td>'
+
+def process_arcane_discovery(arcane_discovery):
+    """ Processes arcane discovery data into a more usable format. """
+
+    # strip any leading and trailing whitespace in all of our arcane discovery's properties
+    for prop in arcane_discovery:
+        if arcane_discovery[prop]:
+            arcane_discovery[prop] = arcane_discovery[prop].strip()
+
+    # turn a restriction sentence into a dict of restriction types
+    restriction = arcane_discovery['restriction']
+    if restriction:
+        restriction_dict = {
+            'bond' : None,
+            'feat' : None,
+            'level': None
+        }
+        bond_regex = re.compile(r'must have chosen (?P<bond>.+?) as your arcane bond')
+        feat_regex = re.compile(r'must have the (?P<feat>.+?) feat')
+        level_regex = re.compile(r'must be at least an? (?P<level>\d+?)[a-zA-Z]{2}-level wizard')
+        for regex in [bond_regex, feat_regex, level_regex]:
+            results = regex.search(restriction)
+            if results:
+                restriction_dict.update(results.groupdict())
+        if restriction_dict['level']:
+            restriction_dict['level'] = int(restriction_dict['level'])
+        arcane_discovery['restriction'] = restriction_dict
+
+    return arcane_discovery
+
+
+# ------------------------------------------------------------------------------
+# witch hexes
+# ------------------------------------------------------------------------------
+
+HEX_DEFAULTS = {
+}
+
+HEX_REGEX = r''
--- database/scripts/scraper.py
+++ database/scripts/scraper.py
@@ -7,7 +7,7 @@ from resources import *
 
 
 def parse_powers(page_ext, list_regex, power_default = None, power_regexes = None, process_fn = None):
-    """Parse a generic list of powers for the details of each one."""
+    """ Parse a generic list of powers for the details of each one. """
     powers = []
 
     # first, get the contents of the page that lists the powers
@@ -46,58 +46,27 @@ def parse_powers(page_ext, list_regex, power_default = None, power_regexes = Non
     return powers
 
 
-def process_spell(spell):
-    """Processes spell data into a more usable format."""
-
-    # strip any leading and trailing whitespace in all of our spell's properties
-    for prop in spell:
-        if spell[prop]:
-            spell[prop] = spell[prop].strip()
-
-    # turn a spell level string into a dict of classes and spell levels
-    level_regex = re.compile(r'(?P<class0>.+?)(?:/(?P<class1>.+))? (?P<level>\d+)')
-    level_dict = {}
-    for level_pair in spell['level'].split(', '):
-        results = level_regex.search(level_pair)
-        if results:
-            match = results.groupdict()
-            level_dict[match['class0']] = int(match['level'])
-            if match['class1']:
-                level_dict[match['class1']] = int(match['level'])
-    spell['level'] = level_dict
-
-    # turn a components string into a list of components and a dict of any accompanying details
-    component_split_regex = re.compile(r', (?![^()]*\))')
-    component_regex = re.compile(r'(?P<type>\w+)(?: \((?P<details>.+?)\))?')
-    components_list = []
-    components_dict = {}
-    components = component_split_regex.split(spell['components'])
-    for component in components:
-        results = component_regex.search(component)
-        if results:
-            match = results.groupdict()
-            components_list.append(match['type'])
-            if match['details'] is not None:
-                components_dict[match['type']] = match['details']
-    spell['components'] = {
-        'list': components_list,
-        'details': components_dict
-    }
-
-    return spell
+def save_json(json_data, filename):
+    """ Write our parsed data to a json file for importing into the database. """
+    with open(filename, 'w') as outfile:
+        data_string = json.dumps(json_data, sort_keys = True, indent = 4, ensure_ascii = False).encode('utf-8')
+        outfile.write(data_string)
 
 
 def main():
-    """Runs the main parsing function on each list of powers and saves the resulting data."""
+    """ Runs the main parsing function on each list of powers and saves the resulting data. """
 
+    # all spells
     spells = parse_powers('Spells.aspx?Class=All', SPELL_LIST_REGEX, SPELL_DEFAULTS, SPELL_REGEXES, process_spell)
-    # hexes = parse_powers('WitchHexes.aspx', HEX_LIST_REGEX, HEX_DEFAULTS, HEX_REGEXES, process_hex)
-    # etc. (yay generalization)
+    save_json(spells, 'spells.json')
 
-    # write our parsed data to a json file for importing into the database
-    with open('spells.json', 'w') as outfile:
-        data_string = json.dumps(spells, sort_keys = True, indent = 4, ensure_ascii = False).encode('utf-8')
-        outfile.write(data_string)
+    # wizard arcane discoveries
+    arcane_discoveries = parse_powers('WizardArcaneDiscoveries.aspx', ARCANE_DISCOVERY_REGEX, ARCANE_DISCOVERY_DEFAULTS, None, process_arcane_discovery)
+    save_json(arcane_discoveries, 'arcane_discoveries.json')
+
+    # witch hexes
+    # hexes = parse_powers('WitchHexes.aspx', HEX_REGEX, HEX_DEFAULTS, None, None)
+    # save_json(hexes, 'hexes.json')
 
 
 if __name__ == '__main__':
