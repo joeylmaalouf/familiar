745ba3757d6dfb6c3cbaf72eec6c0a5ce20d42fd
dbishop <dbishop.rwc@gmail.com>
2018-02-16 17:27:38 -0500

    basic spell adding interface

--- firebase/public/AddSpells.js
+++ firebase/public/AddSpells.js
@@ -1,21 +1,67 @@
 $(document).ready(function() {
-  console.log("Loaded");
   $("#invalidFiletype").hide();
+  $("#spinner").hide();
+  $("#error").hide();
+  $("#preview").hide();
+
+  var spell_data;
 
   $("#jsonSelect").on("change", event => {
     $("#invalidFiletype").hide();
+    $("#spinner").hide();
+    $("#error").hide();
+
     var file = event.target.files[0];
-    console.log(file);
     if (file.name.substr((file.name.lastIndexOf('.') + 1)) === "json") {
+      spell_data = file;
       var reader = new FileReader();
       reader.onload = handleJSON;
-      reader.readAsText(event.target.files[0]);
+      reader.readAsText(spell_data);
     } else {
       $("#invalidFiletype").show();
     }
   });
+
+  function handleJSON(event) {
+    spell_data = JSON.parse(event.target.result);
+    displaySpellData(spell_data);
+  }
+
+  $("#submit").click(event => {
+    if (spell_data != null) {
+      $("#spinner").show();
+      $("#file-chooser").hide();
+      $.post("/spells", {spells: spell_data}, onSuccess).fail(error => {
+        $("#spinner").hide();
+        $("#error").text(error.responseText);
+        $("#error").show();
+      });
+    }
+  });
 });
 
-function handleJSON(event) {
-  console.log(JSON.parse(event.target.result));
+function onSuccess(data, status, xhr) {
+  console.log(data, status, xhr);
+  $("#spinner").hide();
+  $("#file-chooser").show();
+  if (data.success === "true") {
+    alert("Spells Added");
+  } else {
+    $("#error").text(data.error);
+    $("#error").show();
+  }
+}
+
+function displaySpellData(data) {
+  console.log(data);
+  var list = $("<ul>");
+  if ($.isArray(data)) {
+    $.each(data, (index, spell) => {
+      list.append($("<li>").text(spell.name));
+    });
+  } else {
+    list.append($("<li>").text(data.name));
+  }
+  $("#preview-list").html(list);
+  $("#preview").show();
 }
