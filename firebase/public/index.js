2fe5188186552de93edaaf27011e2f5454373627
dbishop <dbishop.rwc@gmail.com>
2018-02-16 18:30:11 -0500

    navbar

---
+++ firebase/public/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+  try {
+    let app = firebase.app();
+    let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
+    document.getElementById('load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
+  } catch (e) {
+    console.error(e);
+    document.getElementById('load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+  }
+});
