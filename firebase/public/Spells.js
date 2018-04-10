b90043865905be3f7618770d08949823504fc01e
Joey L. Maalouf <joeylmaalouf@gmail.com>
2018-04-10 17:58:16 -0400

    37: removed dummy spells, properly use ones pulled from database

--- firebase/public/Spells.js
+++ firebase/public/Spells.js
@@ -1,12 +1,16 @@
 var prepareSpell = (spell) => {
-  spell.levelstr = Object.keys(spell.level).sort().map((spellClass) => {
-    return `${spellClass}: ${spell.level[spellClass]}`;
-  }).join(", ");
+  if (spell.hasOwnProperty("level")) {
+    spell.levelstr = Object.keys(spell.level).sort().map((spellClass) => {
+      return `${spellClass}: ${spell.level[spellClass]}`;
+    }).join(", ");
+  }
 
-  spell.componentstr = spell.components.list.sort().map((component) => {
-    var detail = spell.components.details[component];
-    return `${component}${detail ? ` (${detail})` : ""}`;
-  }).join(", ");
+  if (spell.hasOwnProperty("components")) {
+    spell.componentstr = spell.components.list.sort().map((component) => {
+      var detail = spell.components.details[component];
+      return `${component}${detail ? ` (${detail})` : ""}`;
+    }).join(", ");
+  }
 
   Object.keys(spell).forEach((key) => {
     if (spell[key] === null || spell[key] === undefined) {
@@ -70,70 +74,11 @@ $(document).ready(() => {
   $("#close-spell").click((event) => {
     $("#spell-card").fadeOut();
   });
-  // TODO: call spells route, iterate over them and addCard each
-  var spells = [
-    {
-      "casttime": "1 minute",
-      "components": {
-        "details": {
-          "M": "a flask of holy water worth 25 gp"
-        },
-        "list": [
-          "V",
-          "S",
-          "M",
-          "DF"
-        ]
-      },
-      "duration": "24 hours",
-      "level": {
-        "cleric": 2,
-        "inquisitor": 2,
-        "oracle": 2,
-        "paladin": 2,
-        "warpriest": 2
-      },
-      "link": "SpellDisplay.aspx?ItemName=Abeyance",
-      "longdesc": "Abeyance suppresses the effects of a single curse on a creature. It does not restore any damage or drain that might have been caused by the curse. Abeyance cannot suppress curses that cannot be removed by remove curse, but it can suppress curses such as lycanthropy that require remove curse along with additional measures. An individual curse can be suppressed only once by abeyance, even if cast by a different caster.  Abeyance does not allow a creature to divest itself of cursed objects, though it can suppress a curse contracted from an object.",
-      "name": "Abeyance",
-      "range": "touch",
-      "restriction": null,
-      "save": "Will negates (harmless)",
-      "school": "abjuration",
-      "shortdesc": "Suppress the effects of a single curse.",
-      "spellres": "yes (harmless)",
-      "target": "creature touched"
-    },
-    {
-      "casttime": "2 minutes",
-      "components": {
-        "details": {
-          "M": "a flask of holy water worth 50 gp"
-        },
-        "list": [
-          "V",
-          "M"
-        ]
-      },
-      "duration": "4 minutes",
-      "level": {
-        "sorcerer": 2,
-        "wizard": 2
-      },
-      "link": "SpellDisplay.aspx?ItemName=Abeyance",
-      "longdesc": "Abeyance2 suppresses the effects of a single curse on a creature. It does not restore any damage or drain that might have been caused by the curse. Abeyance cannot suppress curses that cannot be removed by remove curse, but it can suppress curses such as lycanthropy that require remove curse along with additional measures. An individual curse can be suppressed only once by abeyance, even if cast by a different caster.  Abeyance does not allow a creature to divest itself of cursed objects, though it can suppress a curse contracted from an object.",
-      "name": "Abeyance2",
-      "range": "touch",
-      "restriction": null,
-      "save": "Will negates (harmless)",
-      "school": "abjuration",
-      "shortdesc": "Suppress the effects of a single curse.",
-      "spellres": "yes (harmless)",
-      "target": "creature touched"
-    }
-  ];
-  spells.forEach((spell) => {
-    addCard(prepareSpell(spell));
+  $.get("/spells").then((data) => {
+    data.forEach((spell) => {
+      addCard(prepareSpell(spell.data));
+    });
   });
-  // TODO: if filter is applied, hide anything that doesn't match
 });
+
+// TODO: if filter is applied, hide anything that doesn't match
